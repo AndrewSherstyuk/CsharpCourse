bf75070bc31c0a7d7af41506891f23b872806330
AndrewSherstyuk <sherstyuk26@gmail.com>
2021-03-04 18:16:29 +0200

    added task 10 for the Lesson 4 homework

--- Lesson4_Homework/Task10/Program.cs
+++ Lesson4_Homework/Task10/Program.cs
@@ -10,6 +10,62 @@ namespace Task10
     {
         static void Main(string[] args)
         {
+        Start:
+
+            Console.WriteLine("\nPlease enter operand1. Use comma as the decimal point");
+
+            double operand1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter operand2. Use comma as the decimal point");
+
+            double operand2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please select the operation to apply to operand1 and operand2. \nType \"+\" for SUM, \"-\" for SUBSTRACTION, \"*\" for MULTIPLICATION and \"/\" for DIVISION");
+
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            if (operation != 0)
+            {
+                switch (operation)
+                {
+                    case ('+'):
+                        {
+                            Console.WriteLine($"Result is {operand1 + operand2}");
+                            break;
+                        }
+                    case ('-'):
+                        {
+                            Console.WriteLine($"Result is {operand1 - operand2}");
+                            break;
+                        }
+                    case ('*'):
+                        {
+                            Console.WriteLine($"Result is {operand1 * operand2}");
+                            break;
+                        }
+                    case ('/'):
+                        {
+                            if (operand2 == 0)
+                            {
+                                Console.WriteLine("Cannot divide by zero");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Result is {operand1 / operand2}");
+                                break;
+                            }
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Please enter \'+\', \'-\', \'*\', \'/\' to perform an operation, or \'0\' for exit");
+                            break;
+                        }
+                }
+                goto Start;
+            }
+
+            else { }   
         }
     }
 }
