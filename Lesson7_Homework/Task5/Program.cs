4317d24219a8a66f8c90b78c1b3d833f873d380a
AndrewSherstyuk <sherstyuk26@gmail.com>
2021-03-22 14:16:36 +0200

    Refactored Task5 code

--- Lesson7_Homework/Task5/Program.cs
+++ Lesson7_Homework/Task5/Program.cs
@@ -116,20 +116,22 @@ namespace Task5
         public Figure(Point point1, Point point2, Point point3)
         {
             points = new Point[3];
+
             name = String.Concat(point1.PointName, point2.PointName, point3.PointName);
+
             points[0] = point1;
             points[1] = point2;
             points[2] = point3;
         }
 
-        public Figure(Point point1, Point point2, Point point3, Point point4) 
-        {
-            points = new Point[4];
+        public Figure(Point point1, Point point2, Point point3, Point point4) : this(point1, point2, point3)
+        {            
+            Point[] p = new Point[4];
+            points.CopyTo(p, 0);
+            p[3] = point4;
+            points = p;
+
             name = String.Concat(point1.PointName, point2.PointName, point3.PointName, point4.PointName);
-            points[0] = point1;
-            points[1] = point2;
-            points[2] = point3;
-            points[3] = point4;
         }
 
         private double LengthCalc(Point a, Point b)
